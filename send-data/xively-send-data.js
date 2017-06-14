ed870cede97243f1ee40a51561e266542f366ef3
koustabhdolui <koustabh.here@gmail.com>
2017-06-14 16:16:39 +0200

    Changed api endpoint and idmurl to IP for testing agile-sdk calls

--- send-data/xively-send-data.js
+++ send-data/xively-send-data.js
@@ -205,8 +205,8 @@ module.exports = function(RED) {
 
       var storeCredentials = function(msg,credentials){
         var token = msg.token;
-        var api = 'http://agilegw.local:8000'
-        var idmurl = 'http://agilegw.local:3000';
+        var api = 'http://192.168.8.61:8000'
+        var idmurl = 'http://192.168.8.61:3000';
         var agile = require('agile-sdk')({
         api: api,
         idm: idmurl,
